ca6a1f065c6785751cdc4d999bc009be1a74a7b0
Pablo Muñoz <26773480+munozp@users.noreply.github.com>
2019-07-02 19:56:09 +0200

    Initial commit
    First functional version of the script.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# plotlytimelines
- A simple python script to create timelines visualizations with plotly
+# PlotlyTimelines
+A simple python script to create timelines visualizations with plotly
+
+Requirements:
+- python 3.x
+- plotly
+- web browser
+
+Usage:
+python plotlytimelines.py timelineA,action1,start,duration,color;timelineB,action2,start,duration,color... outfile
+
+Colors use hexadecimal format (e.g. A9C308, no '#' symbol)
+Example:
+Output filename is also used as chart title
+
+python plotlytimelines.py UGV,Go-Vt[100-600],0,180.2776,8FEC71;UGV,Wait,180.2776,4,9E9FE8;UGV,Go-Vl[204-600],184.2776,104.7919,8FEC71;UGV,Wait,289.0695,42.0785,9E9FE8;UGV,Go-Vt[248-600],331.148,43.609,8FEC71;UGV,Wait,374.757,5,9E9FE8;UGV,Go-Vl[300-600],379.757,51.599,8FEC71;UGV,Wait,431.3561,96.6229,9E9FE8;UGV,Go-Vi[200-450],527.9789,180.2776,8FEC71;UAV-1,Idle,0,708.2565,DEF4FB;UAV-2,Idle,0,180.2776,DEF4FB;UAV-2,Take-Off,180.2776,4,00D0A3;UAV-2,FlyTo-[150-640],184.2776,64.0312,2ECDEA;UAV-2,Delivery,248.3088,5,E4DB28;UAV-2,Fly-[204-600],253.3088,67.8392,2ECDEA;UAV-2,Land-Vl-[204-600],321.148,10,E9B11B;UAV-2,Charging,331.148,43.609,A81500;UAV-2,Take-Off,374.757,4,00D0A3;UAV-2,FlyTo-[260-660],378.757,61.1109,2ECDEA;UAV-2,Delivery,439.8679,5,E4DB28;UAV-2,Fly-[300-600],444.8679,72.111,2ECDEA;UAV-2,Land-Vl-[300-600],516.9789,11,E9B11B,;UAV-2,Charging,527.9789,152.2219,A81500;UAV-2,Idle,680.2008,28.0557,DEF4FB plotlytimelinesTest
---
+++ plotlytimelines.py
@@ -0,0 +1,47 @@
+import sys
+from datetime import datetime
+from datetime import timedelta
+import plotly
+import plotly.figure_factory as ff
+
+if len(sys.argv) != 3 or ',' not in sys.argv[1] or ';' not in sys.argv[1]:
+    print('Required two parameters: timelineA,action1,start,duration,color;timelineB,action2,start,duration,color... '
+          'outputfile'
+          '\nColors use hexadecimal format (e.g. A9C308)'
+          '\nOutput filename is also used as chart title')
+    exit(-1)
+
+t0 = datetime.now()
+timeformat = '%Y-%d-%m %H:%M:%S.%f'
+timelines = []  # Unique timelines names (ordered)
+plan = []  # Complete plan
+colors = {}  # Colors (one per action)
+
+# Setup actions for filling timelines
+for act in sys.argv[1].split(';'):
+    tmp = act.split(',')
+    start = t0 + timedelta(seconds=float(tmp[2]))
+    finish = start + timedelta(seconds=float(tmp[3]))
+    duration = (finish - start).total_seconds()
+    if not tmp[0] in colors:
+        colors[tmp[1]] = 'rgb{}'.format(tuple(int(tmp[4][i:i+2], 16) for i in (0, 2, 4)))
+    plan.append(dict(Task=tmp[0], Start=start, Finish=finish, Resource=tmp[1],
+                     Description='{} [{:0.0f}s]'.format(tmp[1], duration)))
+    if not tmp[0] in timelines:
+        timelines.insert(0, tmp[0])
+
+# Create figure data
+height = len(timelines) * 200
+fig = ff.create_gantt(plan, width=1920, height=height, colors=colors, index_col='Resource', group_tasks=True,
+                      title=sys.argv[2], showgrid_x=True, showgrid_y=True)
+
+# Setup labels
+fig['layout']['annotations'] = []
+dy = 30
+for act in plan:
+    dy = -dy
+    fig['layout']['annotations'].append(dict(x=act['Start'], y=timelines.index(act['Task']), text=act['Resource'],
+                                             showarrow=True, font=dict(color='black'), ay=dy))
+
+# Create graph
+plotly.offline.plot(fig, filename='{}.html'.format(sys.argv[2]))
